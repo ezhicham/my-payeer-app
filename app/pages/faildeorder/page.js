505026a5b75d2aa935f798b5eb2946b300dd8a3b
ezhicham <zghariservices@gmail.com>
2025-02-04 02:27:20 +0100

    add fail and success pages

---
+++ app/pages/faildeorder/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='bg-red-300 text-red-600 p-2 rounded-sm w-1/3 mx-auto'>failed operation</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ app/pages/successorder/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='bg-green-300 text-green-600 p-2 rounded-sm w-1/3 mx-auto'>success operation</h1>
+    </div>
+  )
+}
+
+export default page
